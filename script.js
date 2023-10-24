5cc4e599e449638755c98404de522d3e105ebef6
SaiNirmal013 <135800422+SaiNirmal013@users.noreply.github.com>
2023-10-24 16:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuIcon = document.querySelector(".menu-icon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopButton = document.getElementById("scrollToTopButton");
+
+    
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            scrollToTopButton.style.display = "block";
+        } else {
+            scrollToTopButton.style.display = "none";
+        }
+    });
+
+});
